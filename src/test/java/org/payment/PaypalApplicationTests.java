f1cb35b9fe3e9d21ec9fee16eb141cbe58271ed2
Amador <amasabcar@outlook.com>
2024-06-03 10:15:04 +0200

    initial commit

---
+++ src/test/java/org/payment/PaypalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
