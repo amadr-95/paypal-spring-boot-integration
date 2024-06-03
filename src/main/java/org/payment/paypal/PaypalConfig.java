eff37fc481253fe4646b059c2d638fd9a040ed01
Amador <amasabcar@outlook.com>
2024-06-03 11:42:08 +0200

    add @Value annotation to properties

--- src/main/java/org/payment/paypal/PaypalConfig.java
+++ src/main/java/org/payment/paypal/PaypalConfig.java
@@ -1,16 +1,18 @@
 package org.payment.paypal;
 
 import com.paypal.base.rest.APIContext;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "application.paypal")
 public class PaypalConfig {
 
+    @Value("${application.paypal.clientId}")
     private String clientId;
+    @Value("${application.paypal.clientSecret}")
     private String clientSecret;
+    @Value("${application.paypal.mode}")
     private String mode;
 
     @Bean
